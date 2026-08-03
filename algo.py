90b1ed89a1fc430f47584c3e89fb50df7c85fc9d
themba <20204166@hope.ac.uk>
2026-08-03 15:31:22 +0100

    first tests

---
+++ algo.py
@@ -0,0 +1,18 @@
+import random 
+
+final = 0
+number = 0
+
+number = int(input("Enter a number: ")) 
+
+def generate_random_number() ->int:
+    return random.randint(1, 1000)
+
+def analyze_number(number: int) -> str:
+    final = generate_random_number()
+    if final % 2 == 0:
+        number = final
+        return "Less"
+    if final % 2 == 1:
+        return "Greater"
+    return f"The number is {number} and the random number is {final}"
