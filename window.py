3e7c4921c38d2eef1831509b8fecbe05fe081530
themba <20204166@hope.ac.uk>
2026-08-03 17:44:21 +0100

    Update window.py

--- window.py
+++ window.py
@@ -1,24 +1,23 @@
 import tkinter as tk 
 
-from algo import generate_random_number, analyze_number
+import algo
 
 class Appwindow:
-    def __init__(self, master):
+    def __init__(self) -> None:
 
-        Pd = str(pady=int(5))
-        self.master = master
+        self.master = tk.Tk()
         self.master.title("Number Analyzer")
         self.master.geometry("400x200")
 
         self.number_entry = tk.Entry(self.master)
-        self.number_entry.pack(Pd)
+        self.number_entry.pack(pady=5)
 
         self.analyze_button = tk.Button(
             self.master, 
             text="Analyze", 
             command=self.analyze_number
         )
-        self.analyze_button.pack(Pd)
+        self.analyze_button.pack()
 
         self.random_button = tk.Button(
             self.master, 
@@ -26,7 +25,7 @@ class Appwindow:
             command=self.generate_random_number
         )
 
-        self.random_button.pack(Pd)
+        self.random_button.pack(pady=10)
 
         def handle_analyze(self) -> None:
             text = self.number_entry.get()
@@ -45,7 +44,7 @@ class Appwindow:
 
             self.number_entry.delete(0, tk.END)
             self.number_entry.insert(0, str(result))
-            result = generate_random_number(NumberAnalyzer(self.number_entry.get()))
+            result = generate_random_number(int(self.number_entry.get()))
 
             self.result_label.config(text=f"Random number generated: {result}")
 
